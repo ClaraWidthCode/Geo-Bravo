c978fd7c04f2a4f8348652065b386cdbf059e2be
ClaraWidthCode <clara.restrepo454@pascualbravo.edu.co>
2025-09-22 17:56:23 -0500

    Fix: merge YulioDev, AppRouter working and homepage connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./config/router";
 import CustomNavBar from "./Components/CustomNavbar";
 import Footer from "./Components/Footer";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <div>
-        <CustomNavBar />
-        <AppRouter />
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <div>
+      <CustomNavBar />
+      <AppRouter />
+      <Footer />
+    </div>
   );
 };
 
 export default App;
-
-
-
-
-
-
-
-
