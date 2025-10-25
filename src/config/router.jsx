e4f3a4eaadc91c31042306195dc660f74488d980
ClaraWidthCode <clara.restrepo454@pascualbravo.edu.co>
2025-10-25 17:01:48 -0500

    Add modulos funcion de area bajo la curva y area entre curvas

--- src/config/router.jsx
+++ src/config/router.jsx
@@ -4,6 +4,8 @@ import HomePage from "../pages/homePage.jsx";
 import Lineal from "../Components/GeoGebra/Lineal.jsx";
 import Quadratic from "../Components/GeoGebra/Quadratic.jsx";
 import Cubic from "../Components/GeoGebra/Cubic.jsx";
+import OneCurve from "../Components/GeoGebra/OneCurve.jsx";
+import TwoCurves from "../Components/GeoGebra/TwoCurve.jsx";
 
 const AppRouter = () => {
   return (
@@ -13,6 +15,8 @@ const AppRouter = () => {
       <Route path="/GeoBravo/Lineal" element={<Lineal />} />
       <Route path="/GeoBravo/Quadratic" element={<Quadratic />}></Route>
       <Route path="/GeoBravo/Cubic" element={<Cubic />}></Route>
+      <Route path="/GeoBravo/OneCurve" element={<OneCurve />}></Route>
+      <Route path="/GeoBravo/TwoCurves" element={<TwoCurves />}></Route>
     </Routes>
   );
 };
